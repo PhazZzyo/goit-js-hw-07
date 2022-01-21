c5b8ab11b4c5c63f0fc8e2d5ed3de9564dcf37da
Alex <41620816+PhazZzyo@users.noreply.github.com>
2022-01-21 16:24:27 +0200

    first part completion

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const galleryMarkup = [];
+
+galleryItems.forEach(element => {
+    const galleryItem = document.createElement("div");
+    galleryItem.classList.add("gallery__item");
+
+    const galleryLink = document.createElement("a");
+    galleryLink.classList.add("gallery__link");
+    galleryLink.href = element.original;
+
+    const galleryImage = document.createElement("img");
+    galleryImage.src = element.preview;
+    galleryImage.classList.add("gallery__image");
+    galleryImage.dataset.source = element.original;
+    galleryImage.alt = element.description;
+
+    galleryLink.append(galleryImage);
+    galleryItem.append(galleryLink);
+    galleryMarkup.push(galleryItem);
+});
+
+galleryRef.append(...galleryMarkup);
+
+galleryRef.addEventListener("click", zoomImage);
+
+const zoomImage = (event) => { 
+    event.preventDefault();
+  
+};
