694b1b5a4e2ad3c91d17cea415322340242de2cc
Alex <41620816+PhazZzyo@users.noreply.github.com>
2022-01-24 09:50:03 +0200

    added functioonality
    added to first task escape key close ability

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,6 +4,29 @@ import { galleryItems } from './gallery-items.js';
 const galleryRef = document.querySelector('.gallery');
 const galleryMarkup = [];
 
+const zoomImage = (event) => {
+    const { target } = event;
+        
+    if (target.classList.contains("gallery__image")) {
+        event.preventDefault();
+        let isModalOpen = false;
+        
+        const modalClose = (event) => {
+            if (event.key === "Escape") {
+                instance.close();
+            }            
+        }
+
+        const instance = basicLightbox.create(
+            `<img width="1400" height="900" src="${event.target.dataset.source}">`,
+            { onClose: () => document.removeEventListener("keydown", modalClose)});
+        
+        isModalOpen = instance.show();        
+    
+        if (isModalOpen) { document.addEventListener("keydown", modalClose) };                       
+    }
+}
+
 galleryItems.forEach(element => {
     const galleryItem = document.createElement("div");
     galleryItem.classList.add("gallery__item");
@@ -26,21 +49,3 @@ galleryItems.forEach(element => {
 galleryRef.append(...galleryMarkup);
 
 galleryRef.addEventListener("click", zoomImage);
-
-function zoomImage(event) {
-    const { target } = event;
-    if (target.classList.contains("gallery__image")) {
-        event.preventDefault();
-        basicLightbox.create(`
-		<img width="1400" height="900" src="${event.target.dataset.source}">`).show();
-
-
-        document.addEventListener("keydown", closeModal);
-        function closeModal(event) {
-        if (event.key === "Escape") {
-            basicLightbox.close();
-            document.removeEventListener("keydown", closeModal)
-      }
-    }
-    }
-}
